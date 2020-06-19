d9571b6e64d7bb482baf40d3e9a0c826e31d0167
eder <eder@infotera.com.br>
2020-06-19 09:39:39 -0300

    Done

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require('uuidv4');
 
-// const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,85 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+
+  const { title, url, techs } = request.body;
+
+  const repositorie = { id: uuid(), title, url, techs, likes: 0 }
+
+  repositories.push(repositorie);
+
+  return response.status(200).json(repositorie);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(r => r.id == id);
+
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({ error: "Nenhum repositório  encontrado" });
+  }
+
+  const repositorie = {
+    ...repositories[repositorieIndex],
+    title,
+    url,
+    techs,
+  };
+
+  repositories[repositorieIndex] = repositorie;
+
+  return response.status(200).json(repositorie);
+
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(r => r.id == id);
+
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({ error: "Nenhum repositório  encontrado" });
+  }
+
+  repositories.splice(repositorieIndex, 1);
+
+  return response.status(204).send();
+
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+
+  
+  const { id } = request.params;
+
+  console.log(id);
+
+  const repositorieIndex = repositories.findIndex(r => r.id == id);
+
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({ error: "Nenhum repositório  encontrado" });
+  }
+
+  const repositorie = repositories[repositorieIndex];
+
+  repositorie.likes++;
+
+  repositories[repositorieIndex] = repositorie;
+
+  return response.status(200).json(repositorie);
 });
 
 module.exports = app;
